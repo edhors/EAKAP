5c3a64d8e073c784c7e3f28500aee521cbcaf24a
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2026-02-03 05:40:32 +0400

    feat: Implement user and refresh token services with database initialization as well as exporting them as API's for other modules to use

--- src/shared/userdb_handler/models.py
+++ src/shared/userdb_handler/models.py
@@ -1,31 +1,21 @@
+from uuid import uuid4
+from datetime import datetime
+from typing import Optional
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel, Column, String
 
-from sqlmodel import DateTime, Field,Index, SQLModel, String,Column,Boolean, Uuid
+
 class User(SQLModel, table=True):
-    id: Uuid| None = Field(default=None, primary_key=True)
-    email: EmailStr = Field(
-        sa_column=Column("email", String, unique=True, nullable=False),
-        max_length=255 
-    )
-    hashed_password: String
-    tenant_id: String = Field(Index)
-    is_active:Boolean = Field(default=True)
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    email: str = Field(sa_column=Column("email", String, unique=True, nullable=False))
+    hashed_password: str
+    tenant_id: str = Field(index=True)
+    is_active: bool = Field(default=True)
+
 
 class RefreshToken(SQLModel, table=True):
-    id: Uuid| None = Field(default=None, primary_key=True)
-    user_id: Uuid| None = Field(default = None, foreign_key=User.id)
-    token: String = Field(Index)
-    expires_at: DateTime
-    revoked:Boolean = Field(default=False)
-
-
-#FIX: Needs pydantic checks and all-- implement
-#TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
-#TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
-
-engine = create_engine(
-     "sqlite+libsql:///embedded.db",
-     connect_args={
-         "auth_token": TURSO_AUTH_TOKEN,
-         "sync_url": TURSO_DATABASE_URL,
-     },
-)
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    user_id: str = Field(foreign_key="user.id", index=True)
+    token: str = Field(index=True)
+    expires_at: datetime
+    revoked: bool = Field(default=False)
